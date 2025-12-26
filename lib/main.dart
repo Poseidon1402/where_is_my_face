35a07edee92793c865fef9a4e5d54c774e29c0c5
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2025-12-26 21:02:14 +0300

    feat(ui): Remove debug banner from the main application for a cleaner UI

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Extracteo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
